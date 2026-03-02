908814bd0f9967514fc25b13d2b4e54b79bbd0fc
Alfianwijayanto24 <alvianwijayanto74@gmail.com>
2026-03-02 20:30:32 +0700

    input data

--- Tugastakjilnasiayam.cpp
+++ Tugastakjilnasiayam.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 int D1, D2, luas;
 
+void input()
+{
+    
+    cout << "Masukan Diagonal 1 : ";
+    cin >> D1;
+    
+    cout << "Masukan Diagonal 2 : ";
+    cin >> D2;
+    
+    
+    
+
+}
+
+
 
 
 
